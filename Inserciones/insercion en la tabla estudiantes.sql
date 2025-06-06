0df5e6ff3cbeefd31662b3df21b6640aff6af495
AlexysVera12 <valexys764@gmail.com>
2025-06-05 21:37:15 -0500

    Insert 4 se creo y ejecuto 8 inserts en la tabla estudiantes

---
+++ Inserciones/insercion en la tabla estudiantes.sql
@@ -0,0 +1,35 @@
+drop table estudiantes;
+create table  estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key(cedula)
+);
+
+insert into estudiantes
+values('1029384756','Mariana','Rodríguez','mariana.rodri@example.com','15/04/2002');
+
+insert into estudiantes
+values('1092837465','Carlos Andrés','Pérez','carlos.perez@example.com','22/08/2000');
+
+insert into estudiantes
+values('1234567890','Lucía','Gómez Fernández','lucia.gomez@example.com','01/12/2001');
+
+insert into estudiantes
+values('1122334455','Juan Manuel','Torres','juan.torres@example.com','09/03/1999');
+
+insert into estudiantes
+values('9988776655','Valentina','Castillo Morales','valen.castillo@example.com','17/06/2003');
+
+insert into estudiantes
+values('8877665544','Alejandro','Martínez Rivas','ale.martinez@example.com','30/11/2000');
+
+insert into estudiantes
+values('7766554433','Camila Sofía','Herrera','camila.herrera@example.com','25/05/2002');
+
+insert into estudiantes
+values('6655443322','Diego','Ramírez Vásquez','diego.ramirez@example.com','12/09/2001');
+
+select * from estudiantes
