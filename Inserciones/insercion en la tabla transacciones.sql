b85d0f4b2d0e9b34be4bc42f9984390a3cb43d31
AlexysVera12 <valexys764@gmail.com>
2025-06-06 12:53:52 -0500

    Insert 8 se creo y ejecuto 10 inserts en la tabla transacciones

---
+++ Inserciones/insercion en la tabla transacciones.sql
@@ -0,0 +1,42 @@
+drop table transacciones;
+create table transacciones(
+	codigo int not null,
+	numero_cuenta char(5) not null,
+	monto money not null,
+	tipo char(1) not null,
+	fecha date not null,
+	hora time not null,
+	constraint transacciones_pk primary key(codigo)
+);
+
+insert into transacciones
+values(10001,'34892',1200.50,'C','05/06/2025','09:45');
+
+insert into transacciones
+values(10002,'98431',300,'D','05/06/2025','10:12');
+
+insert into transacciones
+values(10003,'11234',580.75,'C','05/06/2025','11:03');
+
+insert into transacciones
+values(10004,'76129',90.00,'D','05/06/2025','12:30');
+
+insert into transacciones
+values(10005,'23018',420.40,'C','05/06/2025','13:47');
+
+insert into transacciones
+values(10006,'57482',710.00,'D','05/06/2025','14:05');
+
+insert into transacciones
+values(10007,'19384',100.00,'C','05/06/2025','15:15');
+
+insert into transacciones
+values(10008,'38745',250.60,'D','05/06/2025','16:00');
+
+insert into transacciones
+values(10009,'46298',1333.33,'C','05/06/2025','16:42');
+
+insert into transacciones
+values(10010,'83920',50.00,'D','05/06/2025','17:25');
+
+select * from transacciones
