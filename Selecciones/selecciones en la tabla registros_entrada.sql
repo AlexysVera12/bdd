0c2545f5df825b8cf02b590fc877e8d4ebece2ac
AlexysVera12 <valexys764@gmail.com>
2025-06-06 19:24:57 -0500

    Seleccion 4 se crearon 3 selecciones en la tabla registros_entrada

---
+++ Selecciones/selecciones en la tabla registros_entrada.sql
@@ -0,0 +1,42 @@
+drop table registros_entrada;
+create table registros_entrada(
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not null,
+	constraint registro_entrada_pk primary key(codigo_registro) 
+);
+insert into registros_entrada
+values(10001,'1234567890','06/06/2025','06:30');
+
+insert into registros_entrada
+values(10002,'0987654321','06/06/2025','08:05');
+
+insert into registros_entrada
+values(10003,'1122334455','06/06/2025','07:55');
+
+insert into registros_entrada
+values(10004,'2233445566','06/06/2025','08:10');
+
+insert into registros_entrada
+values(10005,'3344556677','06/06/2025','08:03');
+
+insert into registros_entrada
+values(10006,'4455667788','06/06/2025','07:59');
+
+insert into registros_entrada
+values(10007,'5566778899','06/06/2025','08:12');
+
+insert into registros_entrada
+values(10008,'6677889900','06/06/2025','08:07');
+
+insert into registros_entrada
+values(10009,'7788990011','06/06/2025','08:01');
+
+insert into registros_entrada
+values(10010,'8899001122','06/06/2025','08:04');
+
+select * from registros_entrada;
+select cedula_empleado,fecha,hora from registros_entrada;
+select * from registros_entrada where hora between '7:00' and '14:00';
+select * from registros_entrada where hora > '8:00'
